6bd6081bc5dceba9d57e91e62bf950e07875aa98
Dmitry Ovsyanko <do@eludia.ru>
2011-09-11 16:13:32 +0400

    directory layout changed [refactoring is unfinished]

--- compiled/README.txt
+++
@@ -1,3 +0,0 @@
-This directory is for generated js library code.
-
-No file sould be version controlled here except this README.
--- tools/csc.js
+++
@@ -1,4 +0,0 @@
-var that = include  ('coffee-script');
-var file = new File (system.args [1]);
-file.open ('r');
-system.stdout       (that.CoffeeScript.compile (file.read (), {bare: true}));
